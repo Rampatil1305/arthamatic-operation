c0c9e3e55c0964916a1267ac77c59d09e8259c6a
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-05-07 15:15:22 +0530

    main branch

---
+++ Basicoperation.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class Basicoperation {
+    public static void main(String[]args){
+        int a=10,b=20;
+        System.out.println("Addition of a and b is: "+(a+b));
+        System.out.println("Subtraction of a and b is: "+(a-b));
+        System.out.println("Multiplication of a and b is: "+(a*b));
+}
+}
