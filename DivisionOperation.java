1d8967ec897b97293d653b3cef3bc6dfbde30ee9
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-05-07 16:16:02 +0530

    DivisionOperation.java

--- DivisionOperation.java
+++ DivisionOperation.java
@@ -1,23 +1,16 @@
-package operator;
-
-import java.util.Scanner;
-
 public class DivisionOperation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the numerator: ");
-        int a = scanner.nextInt();
-        System.out.print("Enter the denominator: ");
-        int b = scanner.nextInt();
 
-        try {
-            System.out.println("Division of a and b is: " + (a / b));
-        } catch (ArithmeticException e) {
-            System.out.println("Error: Division by zero is not allowed.");
-        } finally {
-            System.out.println("This block always executes.");
+        int a =15;
+        int b =0;
+        try{
+            System.out.println("Division of a and b is: "+(a/b));
+
+        }catch(ArithmeticException e){
+            System.out.println("Division by zero is not allowed.");
         }
-
-        scanner.close();
+        finally{
+            System.out.println("This block is always executed.");
+        }
     }
 }
